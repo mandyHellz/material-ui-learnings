5b81344877881978187997f8a7ac7c3743f72e83
Amanda Nunes <amandanunes@Amandas-Mac-mini.local>
2021-10-27 17:23:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from "@material-ui/core";
+import { PhotoCamera } from "@material-ui/icons";
+
+import useStyles from "./styles";
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const App = () => {
+    const classes = useStyles();
+    return(
+        <>
+            <CssBaseline/>
+            <AppBar position="relative" color="primary">
+                <Toolbar >
+                    <PhotoCamera className={classes.icon}/>
+                    <Typography variant="h6">
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div className={classes.container}>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            My Photos
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit asperiores, molestias iusto repudiandae blanditiis vero, dicta ipsa cumque tempore corporis veritatis quidem voluptate repellat optio ut, ipsum animi? Magni, autem!
+                        </Typography>
+                        <div className={classes.buttons}>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        Call to action
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Secondary action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+                <Container className={classes.cardGrid} maxWidth="md">
+                    <Grid container spacing={4}>
+                    {cards.map(() => (
+                        <Grid item xs={12} sm={6} md={4} lg={3}>
+                            <Card className={classes.card}>
+                                <CardMedia 
+                                    className={classes.cardMedia}
+                                    image="https://source.unsplash.com/random"
+                                    title="Image title"
+                                />
+                                <CardContent className={classes.CardContent}>
+                                    <Typography gutterBottom variant="h5">
+                                        Title
+                                    </Typography>
+                                    <Typography>
+                                        This is a media card. You can use this section to describe the content. 
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">View</Button>
+                                    <Button size="small" color="primary">Edit</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}    
+                    </Grid>
+                </Container>
+            </main>
+            <footer className={classes.footer}>
+                <Typography variant="h6" align="center" gutterBottom>
+                    Footer
+                </Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary">
+                    Something here to get the footer a purpose!
+                </Typography>
+                <Typography variant="subtitle2" align="center" color="textSecondary">
+                    Copyright &copy; <u>Amanda's learnings</u> 2021.
+                </Typography>
+            </footer>
+        </>
+    )
+}
+
+export default App;
